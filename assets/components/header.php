a24b28ad549134953cdda8fc3c2d525d1011d046
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-06-21 16:07:41 +0200

    Adding header and footer separated

---
+++ assets/components/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CandyShop</title>
+
+  <link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+  <script src="./vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script>
+
+  <link rel="stylesheet" href="./assets/css/global.css">
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -3,23 +3,10 @@ include_once "./classes/bdd.php";
 include_once "./classes/request.php";
 
 $request = new Request();
+
+include_once "./assets/components/header.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CandyShop</title>
-
-  <link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-  <script src="./vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js" defer></script>
-
-  <link rel="stylesheet" href="./assets/css/global.css">
-</head>
-
-<body>
-</body>
-
-</html>
+<?php
+include_once "./assets/components/footer.php";
+?>
