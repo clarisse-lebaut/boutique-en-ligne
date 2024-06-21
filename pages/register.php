d609c4e83a7125cfbace33e62218e195d1c825be
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-06-21 16:21:01 +0200

    Adding basic form to create an account (without the functionalities)

--- pages/register.php
+++ pages/register.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="fr">
+<main>
+  <h1>Créer un compte</h1>
+  <form method="post">
+    <label for="firstname">Prénom</label>
+    <input type="text" name="firstname" id="firstname" required>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>CandyShow - S'enregistrer</title>
-</head>
+    <label for="lastname">Nom de famille</label>
+    <input type="text" name="lastname" id="lastname" required>
 
-<body>
-  <h1>S'enregistrer</h1>
-</body>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email" required>
 
-</html>
+    <label for="confirmationEmail">Email de confirmation</label>
+    <input type="email" name="confirmationEmail" id="confirmationEmail" required>
+
+    <label for="confirmationPassword">Mot de passe de confirmation</label>
+    <input type="text" name="confirmationPassword" id="confirmationPassword" required>
+
+    <input type="submit" value="S'enregistrer">
+  </form>
+</main>
