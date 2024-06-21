b5ee54f5eef04b5cd03ebfcf4bafc097a6861583
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-06-21 16:29:39 +0200

    Adding page title to the page

--- pages/register.php
+++ pages/register.php
@@ -1,5 +1,24 @@
+<?php
+if (false) {
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+}
+
+include_once "../classes/account.php";
+
+include_once "../classes/bdd.php";
+include_once "../classes/request.php";
+
+$request = new Request();
+
+$pageTitle = "Créer un compte";
+include_once "../assets/components/header.php";
+?>
+
 <main>
   <h1>Créer un compte</h1>
+
   <form method="post">
     <label for="firstname">Prénom</label>
     <input type="text" name="firstname" id="firstname" required>
@@ -19,3 +38,7 @@
     <input type="submit" value="S'enregistrer">
   </form>
 </main>
+
+<?php
+include_once "../assets/components/footer.php";
+?>
