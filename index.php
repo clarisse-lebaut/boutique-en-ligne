a981761cf8e27a86217ff4c8c6ca5ab97d0a7ce2
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-06-21 20:25:14 +0200

    Moving includes and the request object into the header to generate only one object

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-include_once "./classes/bdd.php";
-include_once "./classes/request.php";
-
-$request = new Request();
-
 $pageTitle = "Accueil";
 include_once "./assets/components/header.php";
 ?>
