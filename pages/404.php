23e7f246ff0469e809b48fd7d74d12baa5fd7ed3
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-06-24 14:14:53 +0200

    re order files - add css files for 404 and separate csss and php to get a better lisibilty

--- pages/404.php
+++ pages/404.php
@@ -1,62 +1,18 @@
+<?php
+$pageTitle = "404";
+
+// permet d'avoir les couleurs définit dans le global.css
+include "../assets/components/header.php";
+include "../assets/components/footer.php";
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
-    <title>Erreur 404 - Page non trouvée</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #fffafa;
-            text-align: center;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 70px;
-            height: 100vh;
-            margin: 0;
-        }
-
-        h1 {
-            font-size: 36px;
-        }
-
-        .error {
-            background-color: whitesmoke;
-            padding: 20px 50px 20px 50px;
-            border-radius: 20px;
-            box-shadow: 0 0 10px 1px #ffadd9;
-            animation: shadow-pulse 2s infinite alternate;
-        }
-
-        p {
-            font-size: 18px;
-        }
-
-        img {
-            width: 150px;
-        }
-
-        a {
-            text-decoration: none;
-            color: green;
-        }
-
-        a:hover {
-            color: red;
-        }
-
-
-        @keyframes shadow-pulse {
-            0% {
-                box-shadow: 0 0 20px rgba(255, 173, 217, 1);
-            }
-
-            100% {
-                box-shadow: 0 0 70px rgba(255, 0, 217, 100);
-            }
-        }
-    </style>
+    <title><?php echo htmlspecialchars($pageTitle); ?></title>
+    <link rel="stylesheet" href="../assets/css/404.css">
 </head>
 
 <body>
