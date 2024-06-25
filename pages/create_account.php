2039f53e0e06742d9db203550af3db9a0e2389d8
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-06-25 09:50:34 +0200

    add security password + toogle to see and hidde psw

--- pages/create_account.php
+++ pages/create_account.php
@@ -3,16 +3,36 @@ require '../classes/bdd.php';
 require '../classes/request.php';
 
 $message = '';
+$firstname = '';
+$lastname = '';
+$email = '';
 if ($_POST) {
     $firstname = $_POST['firstname'] ?? '';
     $lastname = $_POST['lastname'] ?? '';
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
-    $confirmed_password = $_POST['confirmed_password'] ?? '';
+    $confirm_password = $_POST['confirm_password'] ?? '';
     $role = "User";
 
-    if ($password !== $confirmed_password) {
-        $message = "Les mot de passes ne sont pas identique";
+    // Conditions de sécurité pour le mot de passe
+    $password_min_length = 8;
+    $password_has_uppercase = preg_match('@[A-Z]@', $password);
+    $password_has_lowercase = preg_match('@[a-z]@', $password);
+    $password_has_number = preg_match('@[0-9]@', $password);
+    $password_has_special_char = preg_match('@[^\w]@', $password);
+
+    if ($password !== $confirm_password) {
+        $message = 'Les mots de passe ne sont pas identique';
+    } elseif (strlen($password) < $password_min_length) {
+        $message = 'Le mot de passe doit contenir au moins ' . $password_min_length . ' caractères.';
+    } elseif (!$password_has_uppercase) {
+        $message = 'Le mot de passe doit contenir au moins une lettre majuscule.';
+    } elseif (!$password_has_lowercase) {
+        $message = 'Le mot de passe doit contenir au moins une lettre minuscule.';
+    } elseif (!$password_has_number) {
+        $message = 'Le mot de passe doit contenir au moins un chiffre.';
+    } elseif (!$password_has_special_char) {
+        $message = 'Le mot de passe doit contenir au moins un caractère spécial.';
     } else {
         $request = new Request();
         $result = $request->addUser($firstname, $lastname, $email, $password);
@@ -36,32 +56,57 @@ if ($_POST) {
     <?php if ($message): ?>
         <p><?php echo htmlspecialchars($message); ?></p>
     <?php endif; ?>
+    <style>
+        .toggle-password {
+            cursor: pointer;
+        }
+    </style>
     <form action="" method="POST">
         <div>
             <label for="firstname">Nom</label>
-            <input type="text" id="firstname" name="firstname" required>
+            <input type="text" id="firstname" name="firstname" value="<?php echo htmlspecialchars($firstname); ?>"
+                required>
         </div>
         <div>
             <label for="lastname">Prénom</label>
-            <input type="text" id="lastname" name="lastname" required>
+            <input type="text" id="lastname" name="lastname" value="<?php echo htmlspecialchars($lastname); ?>"
+                required>
         </div>
         <div>
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($email); ?>" required>
         </div>
         <div>
             <label for="password">Mot de passe</label>
-            <input type="password" id="password" name="password" required>
+            <div class="input-group-append">
+                <input type="password" id="password" name="password" required>
+                <span class="input-group-text toggle-password" onclick="togglePassword('password')">&#128065;</span>
+            </div>
         </div>
+
         <div>
             <label for="confirm_password">Confirmer le mot de passe</label>
-            <input type="password" id="confirm_password" name="confirm_password" required>
+            <div class="input-group-append">
+                <input type="password" id="confirm_password" name="confirm_password" required>
+                <span class="input-group-text toggle-password"
+                    onclick="togglePassword('confirm_password')">&#128065;</span>
+            </div>
         </div>
         <div>
             <button type="submit">Créer</button>
         </div>
     </form>
     <a href="./home.php">Revenir à la page d'acceuil</a>
+    <script>
+        function togglePassword(fieldId) {
+            var field = document.getElementById(fieldId);
+            if (field.type === "password") {
+                field.type = "text";
+            } else {
+                field.type = "password";
+            }
+        }
+    </script>
 </body>
 
 </html>
