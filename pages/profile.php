0341f34baefa373b6610d0ace0d1f87d1f4a0200
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-06-28 16:27:42 +0200

    Adding css for the connection page and adding a small css to the profile page

--- pages/profile.php
+++ pages/profile.php
@@ -6,20 +6,42 @@ $updatedAt = new DateTime($account["updated_at"]);
 $createdAt = new DateTime($account["created_at"]);
 ?>
 
-<main>
-  <h1>Profil</h1>
-  <ul>
-    <li>Prénom: <?= $account["firstname"] ?></li>
-    <li>Nom: <?= $account["lastname"] ?></li>
-    <li>Email: <?= $account["email"] ?></li>
-    <li>Adresse: <?= $account["address"] ?></li>
-    <li>Code postal: <?= $account["zipcode"] ?></li>
-    <li>Compte mise à jour le <abbr
-        title="<?= date_format($updatedAt, $simpleDateFormat) ?>"><?= date_format($updatedAt, $dateFormat) ?></abbr>
-    </li>
-    <li>Compte créer le <abbr
-        title="<?= date_format($createdAt, $simpleDateFormat) ?>"><?= date_format($createdAt, $dateFormat) ?></abbr>
-    </li>
-  </ul>
-  <a href="index.php?page=<?= PAGE_MODIFY_PROFILE ?>" class="btn btn-primary">Modifier mon profil</a>
+<main style="margin-block:100px;">
+  <h1 class="text-center py-4" style="font-family: var(--ff-inknut-antiqua);">Profil</h1>
+
+  <div class="d-flex flex-column w-50 mx-auto my-4" style="gap:25px;">
+    <table class="table table-bordered">
+      <tbody>
+        <tr>
+          <td>Prénom</td>
+          <td><?= $account["firstname"] ?></td>
+        </tr>
+        <tr>
+          <td>Nom de famille</td>
+          <td><?= $account["lastname"] ?></td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td><?= $account["email"] ?></td>
+        </tr>
+        <tr>
+          <td>Adresse</td>
+          <td><?= $account["address"] ?></td>
+        </tr>
+        <tr>
+          <td>Code postal</td>
+          <td><?= $account["zipcode"] ?></td>
+        </tr>
+        <tr>
+          <td colspan="2" class="text-center">Compte mise à jour le <abbr title="<?= date_format($updatedAt, $simpleDateFormat) ?>"><?= date_format($updatedAt, $dateFormat) ?></td>
+        </tr>
+        <tr>
+          <td colspan="2" class="text-center">Compte créer le <abbr title="<?= date_format($createdAt, $simpleDateFormat) ?>"><?= date_format($createdAt, $dateFormat) ?></td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="d-flex justify-content-end">
+      <a href="index.php?page=<?= PAGE_MODIFY_PROFILE ?>" class="btn btn-primary">Modifier mon profil</a>
+    </div>
+  </div>
 </main>
